67c7d37263647dcbbff58e47dd75d54b84b58bc7
Silvia-Blup <silvia@itbss.ac.id>
2026-04-27 00:23:46 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
+Route::get('/mahasiswa-create', [MahasiswaController::class, 'create'])->name('mahasiswa.add');
+Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.save');
+Route::get('/mahasiswa-edit/{id}', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.delete');
